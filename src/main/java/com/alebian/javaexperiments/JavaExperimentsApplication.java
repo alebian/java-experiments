2a0af55f312e9e07d13eb645484ffe39ad4b146a
Alejandro Bezdjian <alebezdjian@gmail.com>
2021-10-11 13:11:35 -0300

    Initial commit

---
+++ src/main/java/com/alebian/javaexperiments/JavaExperimentsApplication.java
@@ -0,0 +1,13 @@
+package com.alebian.javaexperiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaExperimentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaExperimentsApplication.class, args);
+	}
+
+}
